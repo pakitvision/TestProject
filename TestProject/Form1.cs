96f833af01e39e8fb3f52ff3eecaef6cc5b2e26c
Pak IT Vision <pakitvision@gmail.com>
2014-11-26 12:28:00 +0500

    Added Message boxes on Startup

--- TestProject/Form1.cs
+++ TestProject/Form1.cs
@@ -19,6 +19,8 @@ namespace TestProject
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Welcome");
+            MessageBox.Show("Welcome Again");
+            MessageBox.Show("Welcome Again Again");
         }
     }
 }
