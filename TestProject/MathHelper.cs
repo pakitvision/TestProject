6dcda899b41f25ae43bed031e4387b1c5ee4450b
Pak IT Vision <pakitvision@gmail.com>
2014-11-26 11:01:29 +0500

    Added a new class MathHelper
    MathHelper class will be used for calculating mathematical operations.

---
+++ TestProject/MathHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestProject
+{
+    public static class MathHelper
+    {
+        public static int GetSum(int firstVal, int secondVal)
+        {
+            return firstVal + secondVal;
+        }
+        public static int GetDifference(int firstVal, int secondVal)
+        {
+            return firstVal - secondVal;
+        }
+    }
+}
